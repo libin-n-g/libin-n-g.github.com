b9e853ad524b316538df51e968a9a74ece375436
Libin <libinngeorge@gmail.com>
2016-12-16 00:26:44 +0530

    stage2

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$('#introanimate').effect('slide',1000);//slideToggle(1000);
 	$('img').show("slide", { direction: "right" }, 1000,function(){
 	$(this).css("display","block");
+	$(this).css("float","none");
 	});
 	$('.detial').accordion();
 	$('.educationcontent h2').effect('slide',1000);
