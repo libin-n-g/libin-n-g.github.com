eaf74ef558b6294a7e889ba4635fd83e9631eaa8
Libin <libinngeorge@gmail.com>
2016-12-15 12:44:28 +0530

    stage1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # libin-n-g.github.com
-# libin-n-g.github.com
+
---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('#introanimate').effect('slide',1000);//slideToggle(1000);
+	$('img').show("slide", { direction: "right" }, 1000,function(){
+	$(this).css("display","block");
+	});
+	$('.detial').accordion();
+	$('.educationcontent h2').effect('slide',1000);
+	
+
+	
+});
+
+
