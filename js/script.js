c19c711b228850bedbe37c53caea3b560ed7a0b8
Libin <libinngeorge@gmail.com>
2016-12-19 00:25:25 +0530

    stage3.7

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	
 	$('#introanimate').effect('slide',1000);//slideToggle(1000);
 	$('img').show("slide", { direction: "right" }, 1000,function(){
+	$(this).addClass("slideanimate");
 	$(this).css("display","block");
 	$(this).addClass("shakeanimate");
 	$(this).css("float","none");
