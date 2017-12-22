623afcef6ee124770839f0ebdb3863343be41c32
libin-n-g <libinngeorge@gmail.com>
2017-12-22 12:18:12 +0530

    Changes Animations

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ $(document).ready(function(){
 	
 	$('#introanimate').effect('slide',1000);//slideToggle(1000);
 	$('img').show("slide", { direction: "right" }, 1000,function(){
-	$(this).addClass("slideanimate");
+	//$(this).addClass("slideanimate");
 	$(this).css("display","block");
-	$(this).addClass("shakeanimate");
+	//$(this).removeClass("img-circle");
+	$(this).addClass("Pictureanimate");
+	$(this).addClass("img-circle");
 	$(this).css("float","none");
 	});
 	$('.detial').accordion();
